5b9de15f31df7adc24e8141d8729759a46ab66e7
AndreiaJSilva <andreia.silva@estudante.ufla.br>
2024-03-27 21:07:32 -0300

    criaçao de array para guardar palavras

--- frontEnd 1.0/script.js
+++ frontEnd 1.0/script.js
@@ -6,11 +6,15 @@ reconhecimento.interimResults = false;
 reconhecimento.maxAlternatives = 1;
 
 var result = document.getElementById('txtChat');
+var palavrasRecebidas = [];
 
 reconhecimento.onresult = function (event) {
     let tamanho = event.results.length;
     let conteudo = event.results[tamanho-1][0].transcript;
     result.textContent = 'Recebido: ' + conteudo;
+
+    palavrasRecebidas.push(conteudo);
+    console.log(palavrasRecebidas);
 }
 
 document.getElementById('btnGravar').onclick = function(){
